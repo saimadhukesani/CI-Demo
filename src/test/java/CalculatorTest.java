5a476cd19fbeb04fb640b76a4cc32c7d65ee7076
saimadhukesani <saimadhukesani0@gmail.com>
2026-03-13 01:05:33 +0530

    Fixed JUnit test imports

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,30 +1,30 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-@Test
-public void testAdd(){
-Calculator c = new Calculator();
-assertEquals(5,c.add(2,3));
-}
+    @Test
+    void testAdd() {
+        Calculator c = new Calculator();
+        assertEquals(5, c.add(2,3));
+    }
 
-@Test
-public void testSubtract(){
-Calculator c = new Calculator();
-assertEquals(2,c.subtract(5,3));
-}
+    @Test
+    void testSubtract() {
+        Calculator c = new Calculator();
+        assertEquals(1, c.subtract(3,2));
+    }
 
-@Test
-public void testMultiply(){
-Calculator c = new Calculator();
-assertEquals(6,c.multiply(2,3));
-}
+    @Test
+    void testMultiply() {
+        Calculator c = new Calculator();
+        assertEquals(6, c.multiply(2,3));
+    }
 
-@Test
-public void testDivide(){
-Calculator c = new Calculator();
-assertEquals(2,c.divide(6,3));
-}
+    @Test
+    void testDivide() {
+        Calculator c = new Calculator();
+        assertEquals(2, c.divide(6,3));
+    }
 
 }
